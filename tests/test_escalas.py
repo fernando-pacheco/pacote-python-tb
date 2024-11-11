db06f3e488eb4026de4de407196b4b241798ec1c
Fernando Pacheco <fernando.silva@sao.jamef.com.br>
2024-11-11 09:30:21 -0300

    Implementação dos cenários ruins e seus testes

--- tests/test_escalas.py
+++ tests/test_escalas.py
@@ -1,25 +1,84 @@
-import pytest
+from pytest import fixture, mark, raises
 
-import pacote_python_tb.escalas as pptb
+from pacote_python_tb.escalas import *
 
 
-def test_escalas_C_maior():
-    esperado = {
+@fixture
+def mock_retorno_esperado_C_maior():
+    return {
         'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
         'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'],
     }
 
-    resultado = pptb.escalas('C', 'maior')
+
+def test_escalas_C_maior(mock_retorno_esperado_C_maior):
+    esperado = mock_retorno_esperado_C_maior
+    resultado = escala('C', 'maior')
 
     assert resultado == esperado
 
 
-def test_escalas_A_maior():
-    esperado = {
-        'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'],
-        'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'],
-    }
+def test_escala_retorno_7_graus(mock_retorno_esperado_C_maior):
+    tonica = 'c'
+    tonalidade = 'maior'
 
-    resultado = pptb.escalas('A', 'maior')
+    esperado = mock_retorno_esperado_C_maior['graus']
+    resultado = escala(tonica, tonalidade)['graus']
 
     assert resultado == esperado
+
+
+def test_escala_entrada_notas_minusculas(mock_retorno_esperado_C_maior):
+    tonica = 'c'
+    tonalidade = 'maior'
+
+    esperado = mock_retorno_esperado_C_maior
+    resultado = escala(tonica, tonalidade)
+
+    assert resultado == esperado
+
+
+def test_escala_entrada_de_nota_nao_existente():
+    tonica = 'X'
+    tonalidade = 'maior'
+
+    mensagem_erro = f'Essa nota não existe, tente uma dessas: {NOTAS}'
+
+    with raises(ValueError) as error:
+        escala(tonica, tonalidade)
+
+    assert mensagem_erro == error.value.args[0]
+
+
+def test_escala_entrada_de_escala_nao_existente():
+    tonica = 'c'
+    tonalidade = 'tonalidade'
+
+    mensagem_erro = f'Essa escala não existe ou não foi implementada. Tente uma dessas {list(ESCALAS.keys())}'
+
+    with raises(KeyError) as error:
+        escala(tonica, tonalidade)
+
+    assert mensagem_erro == error.value.args[0]
+
+
+@mark.parametrize(
+    'tonica,esperado',
+    [
+        ('C', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
+        ('C#', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
+        ('D', ['D', 'E', 'F#', 'G', 'A', 'B', 'C#']),
+        ('D#', ['D#', 'F', 'G', 'G#', 'A#', 'C', 'D']),
+        ('E', ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#']),
+        ('F', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
+        ('F#', ['F#', 'G#', 'A#', 'B', 'C#', 'D#', 'F']),
+        ('G', ['G', 'A', 'B', 'C', 'D', 'E', 'F#']),
+        ('G#', ['G#', 'A#', 'C', 'C#', 'D#', 'F', 'G']),
+        ('A', ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#']),
+        ('A#', ['A#', 'C', 'D', 'D#', 'F', 'G', 'A']),
+        ('B', ['B', 'C#', 'D#', 'E', 'F#', 'G#', 'A#']),
+    ],
+)
+def test_escala_maior_retorno_correto_notas(tonica, esperado):
+    resultado = escala(tonica, 'maior')
+    assert resultado['notas'] == esperado
